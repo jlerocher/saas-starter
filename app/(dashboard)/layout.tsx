86f5c16da43ba2362149679121bde4adbec01918
jlerocher <jlerocher@gmail.com>
2025-02-04 06:28:12 +0100

    Add ThemeProvider component and integrate it into the dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,23 @@
 import { Header } from "@/components/navigation/header";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <section className="flex flex-col min-h-screen">
-            <Header />
-            {children}
-        </section>
+        <html lang="en" suppressHydrationWarning>
+            <head />
+            <body>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <section className="flex flex-col min-h-screen">
+                        <Header />
+                        {children}
+                    </section>
+                </ThemeProvider>
+            </body>
+        </html>
     );
 }
